cf7c33730157f78b3f4fcc89b7fc9abe260bfd5d
KiiSevenK7 <kevin.maldonado@epn.edu.ec>
2024-01-13 00:49:48 -0500

    Created entities

---
+++ 03-Examen01/app/src/main/java/com/example/a03_exam/Hospital.kt
@@ -0,0 +1,21 @@
+package com.example.a03_exam
+
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+class Hospital (
+        var codigoHospital: Int,
+        var name: String?,
+        var capacityPatient: Int,
+        var ubication: String,
+        var dateFoundation: LocalDate?,
+        var isPublic: Boolean?
+    ) {
+        // Método toString() para facilitar la visualización de datos
+        override fun toString(): String {
+            val dateFormat = SimpleDateFormat("dd-MM-yyyy")
+            val formattedDate = dateFormat.format(dateFoundation)
+
+            return "Hospital( Id=$codigoHospital, Nombre='$name', Capacidad=$capacityPatient, Ubication='$ubication', Fundacion=$formattedDate, isPublic=$isPublic)"
+        }
+}
+
