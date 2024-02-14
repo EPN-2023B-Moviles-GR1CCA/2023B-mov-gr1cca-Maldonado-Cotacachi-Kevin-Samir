fedeefd019042c46c76a211437d51ad1ae554015
KiiSevenK7 <kevin.maldonado@epn.edu.ec>
2024-02-14 14:07:36 -0500

    Implementing menu options

--- 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/LoginActivity.kt
+++ 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/LoginActivity.kt
@@ -1,17 +1,22 @@
 package com.example.whatsapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
 
     private var currentUser: FirebaseUser? = null
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var loadingBar: ProgressDialog
     private lateinit var LoginButton : Button
     private lateinit var PhoneLoginButton: Button
     private lateinit var UserEmail : EditText
@@ -19,17 +24,56 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var NeedNewAccoutnLink : TextView
     private lateinit var ForgetPasswordLink : TextView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
+        currentUser = mAuth.currentUser
         initializeFields();
 
         NeedNewAccoutnLink.setOnClickListener {
             sendUserToRegisterActivity()
         }
 
+        LoginButton.setOnClickListener {
+            AllowUserToLogin()
+        }
+    }
+
+    private fun AllowUserToLogin() {
+        val email = UserEmail.text.toString()
+        val password = UserPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email..", Toast.LENGTH_SHORT).show()
+            return
+
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password..", Toast.LENGTH_SHORT).show()
+            return
+        } else{
+
+            loadingBar.setTitle("Sign in")
+            loadingBar.setMessage("Please wait...")
+            loadingBar.setCanceledOnTouchOutside(true)
+            loadingBar.show()
+
+            mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    sendUserToMainActivity()
+                    Toast.makeText(this, "Logged in Successul..", Toast.LENGTH_SHORT).show()
+                    loadingBar.dismiss()
+                } else {
+                    val message = task.exception?.message ?: "Unknown error"
+                    Toast.makeText(this, "Error: $message", Toast.LENGTH_SHORT).show()
+                    loadingBar.dismiss()
+                }
+            }
+        }
     }
 
     private fun initializeFields(){
@@ -39,7 +83,7 @@ class LoginActivity : AppCompatActivity() {
         UserPassword = findViewById(R.id.login_password)
         NeedNewAccoutnLink =findViewById(R.id.need_new_account_link)
         ForgetPasswordLink =findViewById(R.id.forget_password_link)
-
+        loadingBar = ProgressDialog(this)
     }
 
     override fun onStart() {
--- 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/MainActivity.kt
+++ 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.whatsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
@@ -16,11 +18,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var myTabLayout : TabLayout
     private lateinit var myTabsAccessorAdapter: TabsAccessorAdapter
     private var currentUser: FirebaseUser? = null
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mAuth = FirebaseAuth.getInstance()
+        currentUser = mAuth.currentUser
         mToolbar = findViewById(R.id.main_page_toolbar)
         setSupportActionBar(mToolbar)
         supportActionBar?.title = "WhatsApp"
@@ -47,7 +52,31 @@ class MainActivity : AppCompatActivity() {
         val loginIntent = Intent(this, LoginActivity::class.java)
         startActivity(loginIntent)
         //finish() // Finaliza MainActivity para que no se pueda volver atrás después del inicio de sesión
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+       // return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        super.onOptionsItemSelected(item)
+        if(item.itemId == R.id.main_logout_option){
+            mAuth.signOut()
+            sendUserToLoginActivity()
+        }
+        if(item.itemId == R.id.main_settings_option){
+
+        }
+        if(item.itemId == R.id.main_find_friends_option){
+
+        }
+        return true
+    }
+
 
 }
