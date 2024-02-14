4cad66f1f411180f3ac932e9f980c87cc9909969
KiiSevenK7 <kevin.maldonado@epn.edu.ec>
2024-02-13 20:43:09 -0500

    Authentication Firebase Login and Register users

--- 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/LoginActivity.kt
+++ 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/LoginActivity.kt
@@ -3,16 +3,43 @@ package com.example.whatsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
 
     private var currentUser: FirebaseUser? = null
+    private lateinit var LoginButton : Button
+    private lateinit var PhoneLoginButton: Button
+    private lateinit var UserEmail : EditText
+    private lateinit var UserPassword : EditText
+    private lateinit var NeedNewAccoutnLink : TextView
+    private lateinit var ForgetPasswordLink : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        initializeFields();
+
+        NeedNewAccoutnLink.setOnClickListener {
+            sendUserToRegisterActivity()
+        }
+
+    }
+
+    private fun initializeFields(){
+        LoginButton = findViewById(R.id.login_button)
+        PhoneLoginButton = findViewById(R.id.phone_login_button)
+        UserEmail = findViewById(R.id.login_email)
+        UserPassword = findViewById(R.id.login_password)
+        NeedNewAccoutnLink =findViewById(R.id.need_new_account_link)
+        ForgetPasswordLink =findViewById(R.id.forget_password_link)
+
     }
 
     override fun onStart() {
@@ -28,6 +55,12 @@ class LoginActivity : AppCompatActivity() {
         val loginIntent = Intent(this, MainActivity::class.java)
         startActivity(loginIntent)
         //finish() // Finaliza LoginActivity para que no se pueda volver atrás después de iniciar sesión
-
     }
+
+    private fun sendUserToRegisterActivity(){
+        val registerIntent = Intent(this, RegisterActivity::class.java)
+        startActivity(registerIntent)
+        //finish() // Finaliza LoginActivity para que no se pueda volver atrás después de iniciar sesión
+    }
+
 }
--- 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/RegisterActivity.kt
+++ 06-Deber-03_v2/app/src/main/java/com/example/whatsapp/RegisterActivity.kt
@@ -1,11 +1,93 @@
 package com.example.whatsapp
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.newSingleThreadContext
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var CreateAccountButton : Button
+    private lateinit var UserEmail : EditText
+    private lateinit var UserPassword : EditText
+    private lateinit var AlreadyHaveAccountLink : TextView
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var loadingBar: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mAuth =FirebaseAuth.getInstance()
+
+        initializeFields();
+
+        AlreadyHaveAccountLink.setOnClickListener{
+            sendUserToLoginActivity()
+        }
+
+        CreateAccountButton.setOnClickListener {
+            createNewAccount()
+        }
+
+    }
+
+    private fun createNewAccount() {
+        val email = UserEmail.text.toString()
+        val password = UserPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email..", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password..", Toast.LENGTH_SHORT).show()
+            return
+        } else{
+
+            loadingBar.setTitle("Creating New Account")
+            loadingBar.setMessage("Please wait, while we are creating account for you..")
+            loadingBar.setCanceledOnTouchOutside(true)
+            loadingBar.show()
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        sendUserToLoginActivity()
+                        Toast.makeText(this, "Account Created Successfully..", Toast.LENGTH_SHORT).show()
+                        loadingBar.dismiss()
+                    } else {
+                        val message = task.exception?.message ?: "Unknown error"
+                        Toast.makeText(this, "Error: $message", Toast.LENGTH_SHORT).show()
+                        loadingBar.dismiss()
+
+                    }
+                }
+        }
+
+
+    }
+
+
+    private fun initializeFields(){
+        CreateAccountButton = findViewById(R.id.register_button)
+        UserEmail = findViewById(R.id.register_email)
+        UserPassword = findViewById(R.id.register_password)
+        AlreadyHaveAccountLink =findViewById(R.id.already_have_account_link)
+        loadingBar =  ProgressDialog(this)
+    }
+    private fun sendUserToLoginActivity(){
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+        //finish() // Finaliza LoginActivity para que no se pueda volver atrás después de iniciar sesión
     }
 }
